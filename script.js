09b7a76e4675a7a7facd04685b0a1e166a22d370
Jon Jonker <jonjonker@hotmail.com>
2020-12-23 10:56:14 +0100

    made assingments

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+const array = [
+    { name: "N. Armstrong", profession: "spacecowboy", age: 89 },
+    { name: "H. de Haan", profession: "kippen hypnotiseur", age: 59 },
+    { name: "A. Curry", profession: "kikvorsman", age: 32 },
+    { name: "F. Vonk", profession: "slangenmelker", age: 36 },
+    { name: "B. Bunny", profession: "konijnen uitlaatservice", age: 27 },
+    { name: "Dr.Evil", profession: "digital overlord", age: 56 }
+  ];
+  
+  for (let person of array) {
+    //console.log(person) 
+    //console.log("Dit is de gehele persoon:", [person]);
+    //console.log("Dit is naam:", [person.name]);
+    //console.log("Dit is het geboortjaar:", [person.age] - 2020);
+    //console.log([person.name] + ` is een`, [person.profession]);
+
+    if (person.age >= 50) {
+
+        console.log(person.name)
+    
+    }
+
+  }
+
+
 
